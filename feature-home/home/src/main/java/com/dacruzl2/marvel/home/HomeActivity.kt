17275bc3962d67fdf807ab883141a5a4eda5b753
Leonardo Cruz <leonardo.ocruz@stone.com.br>
2021-12-02 15:27:29 -0300

    feat: create home module

---
+++ feature-home/home/src/main/java/com/dacruzl2/marvel/home/HomeActivity.kt
@@ -0,0 +1,11 @@
+package com.dacruzl2.marvel.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
