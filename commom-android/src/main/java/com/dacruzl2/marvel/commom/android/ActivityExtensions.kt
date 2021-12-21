d75c966ed89ac36f4f5a4176706bb735cfc1e61a
Leonardo Cruz <leonardo.ocruz@stone.com.br>
2021-12-21 13:11:25 -0300

    test: testing usecase

--- commom-android/src/main/java/com/dacruzl2/marvel/commom/android/ActivityExtensions.kt
+++ commom-android/src/main/java/com/dacruzl2/marvel/commom/android/ActivityExtensions.kt
@@ -1,16 +1,15 @@
 package com.dacruzl2.marvel.commom.android
 
-import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentActivity
+import androidx.activity.ComponentActivity
 import com.dacruzl2.marvel.commom.koin.KoinTags
 import org.koin.core.context.loadKoinModules
 import org.koin.core.qualifier.named
 import org.koin.dsl.koinApplication
 import org.koin.dsl.module
 
-fun AppCompatActivity.selfInject() = koinApplication {
+fun ComponentActivity.selfInject() = koinApplication {
     val hostScreen = module {
-        single<FragmentActivity>(named(KoinTags.HOST_ACTIVITY)) {
+        single<ComponentActivity>(named(KoinTags.HOST_ACTIVITY)) {
             this@selfInject
         }
     }
